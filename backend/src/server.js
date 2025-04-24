b846680232cdbbca747cdc9b41bd3f847b12bd55
Steven Cox <Stevencox3406@gmail.com>
2025-04-24 18:59:45 -0400

    Added a readme and testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,41 +1,22 @@
 const express = require("express");
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const skateboardRoutes = require("./routes/skateboardRoutes");
-const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
-// Enable CORS
-app.use(cors()); // Allow all origins by default
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the API!");
-});
-
-// Use the auth routes
+// Routes
 app.use("/auth", authRoutes);
-
-app.use("/api", authRoutes);
-
-// Use the admin routes
 app.use("/admin", adminRoutes);
-
-// Use the skateboard routes
 app.use("/api/skateboards", skateboardRoutes);
 
-// Serve static files from the frontend build folder
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-// Catch-all route to serve the frontend for any unknown routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
