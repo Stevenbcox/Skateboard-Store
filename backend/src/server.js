ae14588291260d12cbe64e692f3265b8fb4c5bd9
Steven Cox <Stevencox3406@gmail.com>
2025-04-24 18:07:40 -0400

    Added a readme and testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,11 @@ const skateboardRoutes = require("./routes/skateboardRoutes");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 // Enable CORS
 app.use(cors()); // Allow all origins by default
 
