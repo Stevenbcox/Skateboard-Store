be982fe6674002b4965b66fd1d4288a8c316e1b5
Steven Cox <Stevencox3406@gmail.com>
2025-04-25 17:50:42 -0400

    Test

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "stevencox",
-  host: "localhost",
-  database: "skateboard_store",
-  password: "Password1", 
-  port: 5432,
+  user: process.env.DB_USER || "stevencox",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "skateboard_store",
+  password: process.env.DB_PASSWORD || "Password1",
+  port: process.env.DB_PORT || 5432,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false, // Enable SSL for Render
 });
 
 module.exports = pool;
